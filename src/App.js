ed94393068fc905c7c31fb8476411d33a1f5e02d
Ambuj-Pandey <78492933+Ambuj-Pandey@users.noreply.github.com>
2023-09-03 15:35:33 +0000

    Fetching Movie details

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
+
+import "./App.css";
+
+const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
+
+const App = () => {
+
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="app">
+      <h1>MovieMania</h1>
     </div>
   );
-}
+};
 
 export default App;
