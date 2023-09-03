03fa3f52782bf85550af46f16a2cd4ebcbe82c30
Ambuj-Pandey <78492933+Ambuj-Pandey@users.noreply.github.com>
2023-09-03 16:35:49 +0000

    Search functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import SearchIcon from "./search.svg";
 
 
 import "./App.css";
@@ -6,10 +7,10 @@ import "./App.css";
 const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("Hera Pheri");
   }, []);
 
   const searchMovies = async (title) => {
@@ -21,6 +22,18 @@ const App = () => {
   return (
     <div className="app">
       <h1>MovieMania</h1>
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
     </div>
   );
 };
